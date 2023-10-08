c3f893053f060f1fcc81e4666cd1e659ba0d5744
george <george@parity.io>
2023-10-08 13:25:19 +0100

    not working json errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import { ThemeProvider } from '@/app/context/ThemeProvider';
 import { PokemonProvider } from '@/app/context/PokemonProvider';
+
+import "regenerator-runtime/runtime";
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
+
 import './globals.css';
 import { Header } from '@/components/header';
+
+const supportedChainIds = [1, 5];
+const connectors = {
+  injected: {},
+};
 
 export default function RootLayout({
   children,
@@ -11,26 +22,31 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ThemeProvider attribute="class" defaultTheme="light">
+        <ThemeProvider attribute="class" defaultTheme="dark">
           <PokemonProvider>
-            <main
-              className="w-full bg-[size:200%_200%]
+            <ThirdwebWeb3Provider
+              connectors={connectors}
+              supportedChainIds={supportedChainIds}
+            >
+              <main
+                className="w-full bg-[size:200%_200%]
               bg-gradient-radial
               from-[#c6e4ff]
               to-[#fff7cf]
               animate-background-flow"
-            >
-              <div
-                className="w-full bg-[size:200%_200%] 
+              >
+                <div
+                  className="w-full bg-[size:200%_200%] 
               dark:bg-gradient-radial
               from-[#121829]
               to-[#193e63]
               animate-background-flow"
-              >
-                <Header />
-                {children}
-              </div>
-            </main>
+                >
+                  <Header />
+                  {children}
+                </div>
+              </main>
+            </ThirdwebWeb3Provider>
           </PokemonProvider>
         </ThemeProvider>
       </body>
---
+++ app/web3auth/index.tsx
@@ -0,0 +1,25 @@
+import { useWeb3 } from "@3rdweb/hooks";
+
+export default function Web3AuthPage() {
+    const {address, chainId, connectWallet, error} = useWeb3();
+    if(error) {
+        return (
+            <>
+                <h1>Error: {error.message}</h1>
+            </>
+        )
+    } else if(address) {
+        return (
+            <>
+                <h1>Connected to {chainId}</h1>
+                <h1>Connected to {address}</h1>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <button onClick={() => connectWallet("injected")}>Connect</button>
+            </>
+        )
+    }
+}
