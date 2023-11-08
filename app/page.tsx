113f076f63eecde54997677649a113e413ee39f2
george <george@parity.io>
2023-11-08 22:26:13 +0000

    fix type error

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
   return (
     <>
       <Banner />
-      <Grid pokemonData={pokemonData} />
+      <Grid pokemonData={pokemonData} pokemonList={[]} />
     </>
   );
 }
